bc2aba64b6b588bb1b5c9c3742f842636c20db82
SFPGroup <joaoramalhosasfpgroup@gmail.com>
2025-06-30 14:27:37 +0100

    Testubg versions

--- frontend/publish.js
+++ frontend/publish.js
@@ -1,18 +1,43 @@
 require('dotenv').config();
 const { build } = require('electron-builder');
 
+if (!process.env.GH_TOKEN) {
+  console.error('❌ GH_TOKEN não definido no .env');
+  process.exit(1);
+}
+
 build({
   config: {
+    appId: 'com.sfpgroup.ticket',
+    productName: 'Sistema Tickets',
+    icon: 'src/images/logo32x32.ico',
+    files: [
+      {
+        from: 'dist/',
+        to: './',
+        filter: ['**/*']
+      },
+      'main.js',
+      'package.json'
+    ],
+    win: {
+      target: 'nsis'
+    },
     publish: [
       {
         provider: 'github',
         owner: 'joaoSFPGROUP',
-        repo: 'ticketSystem'
+        repo: 'ticketSystem',
+        releaseType: 'release'
       }
     ]
   },
   publish: 'always'
-}).catch(err => {
-  console.error('Erro ao publicar:', err);
-  process.exit(1);
-});
+})
+  .then(() => {
+    console.log('✅ Publicação concluída com sucesso!');
+  })
+  .catch(err => {
+    console.error('❌ Erro ao publicar:', err);
+    process.exit(1);
+  });
